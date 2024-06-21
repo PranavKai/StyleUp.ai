a1f19a65c6bfe6f61aa70d7dd96099ff7d9c4835
PranavKai <pranavkaihatsu@gmail.com>
2024-06-22 02:33:32 +0900

    Deploying on vercel

--- server/server.js
+++ server/server.js
@@ -71,6 +71,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(5000, () => {
-    console.log('Server running on http://localhost:5000');
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
