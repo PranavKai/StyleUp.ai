33431611350c34d4457801f12a1578f8d84e6778
PranavKai <pranavkaihatsu@gmail.com>
2024-06-22 19:16:08 +0900

    Server.js file updated fixed API routes

--- server/server.js
+++ server/server.js
@@ -9,6 +9,8 @@ console.log('Environment Variables:');
 console.log('API_KEY:', process.env.API_KEY);  // Add this line to log the API key
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
@@ -70,10 +72,10 @@ app.post('/recommend', async (req, res) => {
 // Serve the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
-
-app.listen(5000, () => {
-    console.log('Server running on http://localhost:5000');
-});
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello, World!' });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
