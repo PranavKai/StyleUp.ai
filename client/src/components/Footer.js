5c8f020a761a37960302174e93a9e272d1a05a6c
PranavKai <pranavkaihatsu@gmail.com>
2024-06-25 12:55:08 +0900

    project update

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,8 +7,6 @@ export default function Footer() {
         <p>&copy; 2024 StyleUp-AI. All rights reserved.</p>
         <p>Developed by Pranav Deshpande</p>
         <div className="space-x-4 mt-2">
-          <a href="https://github.com/PranavKai" className="text-white hover:text-gray-200">GitHub</a>
-          <a href="https://www.linkedin.com/in/yourusername" className="text-white hover:text-gray-200">LinkedIn</a>
         </div>
       </div>
     </footer>
